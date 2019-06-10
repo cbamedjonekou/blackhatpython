e4f0af77bb055b58b4652009293564877de3286b
Chris Amedjonekou <death.thestraw.berry@gmail.com>
2019-06-10 16:41:20 -0400

    example TCP Client update

--- TCP Client POC.py
+++ TCP Client POC.py
@@ -15,9 +15,9 @@ target_host, target_port = "wwww.google.com", 80
 # Then we create our socket object
 # socket.socket() is used to create the socket object
 # It takes two parameters: AF_INET, and SOCK_STREAM
-# AF_INET represents the parameter for the socket domain 
-# By setting AF_INET (Address Family-Internet Networking) as the socket domain parameter 
-# only IP (Internet Protocols) addresses can be used by the socket
+# The AF_INET parameter is saying we are going to use a standard IPv4 address or hostname
+# AF_INET represents the parameter for the socket domain (Address Family-Internet Networking)
+# SOCK_STREAM indicates that this will be a TCP client. 
 # SOCK_STREAM represents the parameter for the socket type
 # Link: https://github.com/python/cpython/blob/3.7/Lib/socket.py
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,4 +26,36 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # For IP sockets, the address is a tuple (host, port).
 address = (target_host, target_port)
 client.connect(address)
+
+
+# The following can also be written as such where adding `b` to the front of the string in sentData treats it as a `bytes parameter`:
+# 
+# ```python
+# sentData = b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n"
+# client.send(sentData)
+# 
+# buffersize = 4096
+# response = client.recv(buffersize)
+# ```
+
+# In[2]:
+
+
+# Once we connect we send some data
+# Sent Data is in the form of a unicode string
+sentData = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n"
+client.send(sentData.encode('utf-8'))
+
+
+# Then we retrieve the response
+buffersize = 4096
+response = client.recv(buffersize).decode('utf-8')
+
+
+# #### Response for sending some data
+
+# In[3]:
+
+
+print(response)
 
